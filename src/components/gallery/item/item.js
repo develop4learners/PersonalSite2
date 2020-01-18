8d1598488dc728e306ee24625780806a558e7aa9
develop4learners <cqr2@cornell.edu>
2020-01-18 11:09:38 -0800

    trying to get pdfs to work in the homepage.json file not quite working

--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import { Title, Copy } from './item.css';
-
-const Item = ({ title, copy, image }) => (
+import { Link } from "gatsby"
+//import Pdf from "../../../../content/Semerjian-Roberto-CSCL-2019.pdf";
+//<a href = {Pdf} does actually work but I wish json would instead...
+const Item = ({ title, copy, image, pdf}) => (
   <figure>
-    <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
+    <Link to={pdf}>
+      <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
+    </Link>
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
@@ -17,6 +21,7 @@ Item.propTypes = {
   title: PropTypes.string,
   copy: PropTypes.string,
   image: PropTypes.object.isRequired,
+  pdf: PropTypes.object.isRequired
 };
 
 export default Item;
