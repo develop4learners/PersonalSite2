6f2c9565527fa14bc7b67b1329d366fdffac47b0
develop4learners <cqr2@cornell.edu>
2020-01-20 10:47:26 -0800

    added ukulele tabs for always will from bright star

--- src/components/download.js
+++ src/components/download.js
@@ -7,6 +7,104 @@ class Download extends Component {
 
     return (
       <div className = "App">
+      <div>
+        <br />
+        Billy:
+        <br />
+        G              D      A        D
+        <br />
+        We’re supposed to be together, I know
+        <br />
+        A              D     B          A
+        <br />
+        I feel it way down deep in my soul
+        <br />
+        G            D       A      D
+        <br />
+        We’re never meant to be apart
+        <br />
+        A           D      A         D
+        <br />
+        I keep you here inside of my heart
+        <br />
+        <br />
+        G       D     G      D
+        <br />
+        I always have, always will
+        <br />
+        G        Em      B?      A
+        <br />
+        Always, always, always will
+        <br />
+        Billy and Margo:
+        <br />
+        G       D     G      D
+        <br />
+        I always have, always will
+        <br />
+        G       Em       A(7)      D
+        <br />
+        Always, always, always will
+        <br />
+        <br />
+        Margo:
+        <br />
+        G                 D      A        D
+        <br />
+        We’re supposed to be together, it’s true
+        <br />
+        A              D          B      A
+        <br />
+        I’ve had my doubts but not about you
+        <br />
+        <br />
+        Billy:
+        <br />
+        G            D       A    D
+        <br />
+        We’re never meant to be apart
+        <br />
+        A           D      A         D
+        <br />
+        I love you now I have from the start
+        <br />
+        <br />
+        Billy and Margo:
+        <br />
+        G       D     G      D
+        <br />
+        I always have, always will
+        <br />
+        G    Em      B?        A
+        <br />
+        Always, always, always will
+        <br />
+        G       D     G      D
+        <br />
+        I always have, always will
+        <br />
+        G       Em       A(7)      D
+        <br />
+        Always, always, always will
+        <br />
+        <br />
+        We’re never meant to be apart
+        <br />
+        I love you now, I have from the start
+        <br />
+        <br />
+        I always have, always will
+        <br />
+        Always, always, always will
+        <br />
+        <br />
+        Ensemble:
+        <br />
+        They always have, always will
+        <br />
+        Always, always, always will
+        <br />
+        </div>
         <a href = {Pdf} target = "_blank" rel="noopener noreferrer">
           <img src={require('../../content/images/Semerjian-Roberto-CSCL-2019.jpg')} alt="Download Pdf"
           width = "750px"/>
