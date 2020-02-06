224c2fcad9908f0dad0933c5c5c1301e7ac8ddf6
develop4learners <cqr2@cornell.edu>
2020-02-05 23:07:28 -0800

    finished the mvp to get the pdfs working, woohoo :)

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Item from 'components/gallery/item';
 import { Container } from './gallery.css';
-const pdfs = [
-  "../../../../content/pdf/Semerjian-Roberto-CSCL-2019.pdf",
-  "../../../../content/pdf/Roberto-Davis-AERA-2020.pdf",
-  "../../../../content/pdf/Roberto-ICLS-2020.pdf"];
+import Pdf1 from "../../../content/pdf/Roberto-ICLS-2020.pdf";
+import Pdf2 from "../../../content/pdf/Semerjian-Roberto-CSCL-2019.pdf";
+import Pdf3 from "../../../content/pdf/Roberto-Davis-AERA-2020.pdf";
+const pdfs = [Pdf1, Pdf2, Pdf3];
 
 const Gallery = ({ items }) => (
   <Container>
--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -3,20 +3,13 @@ import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
 import { Title, Copy } from './item.css';
 import { Link } from "gatsby"
-const pdfs = [
-  "../../../../content/pdf/Semerjian-Roberto-CSCL-2019.pdf",
-  "../../../../content/pdf/Roberto-Davis-AERA-2020.pdf",
-  "../../../../content/pdf/Roberto-ICLS-2020.pdf"];
 
 const Item = ({ title, copy, image}, index) => (
   <figure>
-    <a href = {pdfs[index]} target = "_blank" rel="noopener noreferrer">
       <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
-    </a>
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
-      <div>Some text</div>
     </figcaption>
   </figure>
 );
